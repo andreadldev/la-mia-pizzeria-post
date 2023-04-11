0a5d682fac814b4e69794b93ae44d7efc16bfe64
andreadldev <andrea.delisi2@gmail.com>
2023-04-11 16:41:59 +0200

    Validazione

--- la-mia-pizzeria-post/Models/Pizza.cs
+++ la-mia-pizzeria-post/Models/Pizza.cs
@@ -15,9 +15,15 @@ namespace la_mia_pizzeria_post.Models
 
         [Key]
         public long Id { get; set; }
+        [Url]
         public string Img { get; set; }
+        [Required]
+        [StringLength(25)]
         public string Name { get; set; }
+        [StringLength(100)]
         public string Description { get; set; }
+        [Required]
+        [Range(1, 20)]
         public double Price { get; set; }
     }
 }
